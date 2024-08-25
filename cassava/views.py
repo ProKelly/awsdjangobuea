4b268b0cd5a98d8c4159ab3aac3c90056a7f46d9
Steve Yonkeu <yokwejuste@yahoo.com>
2024-08-25 21:09:23 +0100

    feaT: updates

--- cassava/views.py
+++ cassava/views.py
@@ -1,27 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    html = """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hello World</title>
-        <style>
-            html, body {
-                height: 100%;
-                margin: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-family: Arial, sans-serif;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Hello World!</h1>
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
+    return render(request, 'index.html')
